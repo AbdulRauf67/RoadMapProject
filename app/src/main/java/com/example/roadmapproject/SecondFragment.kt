aed48502d61d2b1261054bcff3e9732009540a7a
Abdul Rauf <a.rauf.bscs.kpr@gmail.com>
2024-02-28 20:39:42 +0500

    call one fragment to another

--- app/src/main/java/com/example/roadmapproject/SecondFragment.kt
+++ app/src/main/java/com/example/roadmapproject/SecondFragment.kt
@@ -35,6 +35,9 @@ class SecondFragment : Fragment() {
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
+        binding.button5.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_firstListFragment)
+        }
     }
 
     override fun onDestroyView() {
