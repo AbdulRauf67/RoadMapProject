7e9a640117a1459ad8e826aad3306471a07df1c9
Abdul Rauf <a.rauf.bscs.kpr@gmail.com>
2024-02-28 18:09:21 +0500

    List Fragment with RecyclerView

--- app/src/main/java/com/example/roadmapproject/MainActivity.kt
+++ app/src/main/java/com/example/roadmapproject/MainActivity.kt
@@ -110,6 +110,10 @@ class MainActivity : AppCompatActivity() {
             var intent=Intent(this,SingleFragmentActivity::class.java)
             startActivity(intent)
         }
+        else if (item.itemId==R.id.ListFragment){
+            var intent=Intent(this, ListFragmentActivity::class.java)
+            startActivity(intent)
+        }
         else{
             Toast.makeText(this,"Copy",Toast.LENGTH_LONG).show()
         }
