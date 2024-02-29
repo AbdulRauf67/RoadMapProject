c86dfea26e908a502f537ee4769e41a821e48f46
Abdul Rauf <a.rauf.bscs.kpr@gmail.com>
2024-02-29 22:27:22 +0500

    Simple ViewPager design

--- app/src/main/java/com/example/roadmapproject/MainActivity.kt
+++ app/src/main/java/com/example/roadmapproject/MainActivity.kt
@@ -118,6 +118,10 @@ class MainActivity : AppCompatActivity() {
             var intent=Intent(this, kotlin_fragments_passingdata_backstack::class.java)
             startActivity(intent)
         }
+        else if (item.itemId==R.id.ViewPagerActivity){
+            var intent=Intent(this, ViewPagerActivity::class.java)
+            startActivity(intent)
+        }
         else{
             Toast.makeText(this,"Copy",Toast.LENGTH_LONG).show()
         }
