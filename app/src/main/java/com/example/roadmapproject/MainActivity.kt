2303308923d86db927eea002e9a6d0c8197c6296
Abdul Rauf <a.rauf.bscs.kpr@gmail.com>
2024-03-03 15:12:42 +0500

    Navigation Drawer added

--- app/src/main/java/com/example/roadmapproject/MainActivity.kt
+++ app/src/main/java/com/example/roadmapproject/MainActivity.kt
@@ -10,13 +10,22 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.PopupMenu
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.roadmapproject.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
 
     private lateinit var binding: ActivityMainBinding
-    @SuppressLint("UseSupportActionBar")
+    @SuppressLint("UseSupportActionBar", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -59,6 +68,18 @@ class MainActivity : AppCompatActivity() {
             }
             pop_upMenu.show()
         })
+
+        toolbar = findViewById(R.id.toolbar2)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout1)
+        navView = findViewById(R.id.nav_view)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.nav_open, R.string.nav_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
 
     }
     //configure option menus
@@ -143,11 +164,36 @@ class MainActivity : AppCompatActivity() {
                 var intent = Intent(this, BottomSheetActivity::class.java)
                 startActivity(intent)
             }
+            R.id.navigationDrawerBar -> {
+                var intent = Intent(this, NavigationDrawerActivity::class.java)
+                startActivity(intent)
+            }
             else -> {
                 Toast.makeText(this,"Copy",Toast.LENGTH_LONG).show()
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_profile -> {
+                Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_messages -> {
+                Toast.makeText(this, "Messages clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_friends -> {
+                Toast.makeText(this, "Friends clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_update -> {
+                Toast.makeText(this, "Update clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_logout -> {
+                Toast.makeText(this, "Sign out clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 
 }
